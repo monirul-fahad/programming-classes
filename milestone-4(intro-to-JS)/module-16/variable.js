1a4540dea89ecb5b5ec275b22f0eda6cef3ceab6
Monirul Fahad <immonirulfahad@gmail.com>
2021-07-31 13:16:16 +0600

    js

---
+++ milestone-4(intro-to-JS)/module-16/variable.js
@@ -0,0 +1,13 @@
+var price = 21;
+var age = 22;
+var temperature = 37;
+
+var laptop = 'asus';
+var city = 'Dhaka';
+
+var name = 'Monirul Fahad';
+var address = 'Tongi para';
+var gf = 'Alia Bhatt';
+
+var pass = true;
+var subscribed = false;
