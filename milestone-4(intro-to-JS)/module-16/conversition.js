0075e42a65741b5aef5b44d3b44f278b0f098554
Monirul Fahad <immonirulfahad@gmail.com>
2021-07-31 18:21:06 +0600

    conversation

---
+++ milestone-4(intro-to-JS)/module-16/conversition.js
@@ -0,0 +1,10 @@
+var firstName = 'Monirul';
+var lastName = 'Fahad';
+
+var fullName = firstName + ' ' + lastName;
+console.log(fullName)
+
+var price1 = '43';
+var price2 = '33';
+
+var totalPrice = price1 + price2;
--- milestone-4(intro-to-JS)/module-16/math-shorthand-OP.js
+++ milestone-4(intro-to-JS)/module-16/math-shorthand-OP.js
@@ -1,4 +1,12 @@
 var price1 = 32;
 var price2 = 8;
 
-var sum = price1 + price2;
+// var sum = price1 + price2;
+// var sub = price2 - price1;
+
+price1 += 10;
+
+price2 -+ 20;
+
+console.log(price1)
+console.log(price2)
