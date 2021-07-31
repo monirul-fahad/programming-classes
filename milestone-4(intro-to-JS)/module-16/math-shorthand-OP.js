599701a9e8273ba213d911db7c10ff012ee09c27
Monirul Fahad <immonirulfahad@gmail.com>
2021-07-31 15:50:08 +0600

    math

---
+++ milestone-4(intro-to-JS)/module-16/math-shorthand-OP.js
@@ -0,0 +1,4 @@
+var price1 = 32;
+var price2 = 8;
+
+var sum = price1 + price2;
---
+++ milestone-4(intro-to-JS)/module-16/mathemitical-op.js
@@ -0,0 +1,26 @@
+var onionPrice = 67;
+var eggPrice = 12;
+var eggQuantity = 7;
+
+var moneyAvailable = 100;
+var orangePrice = 5;
+
+// sum 
+var totalPrice = onionPrice + eggPrice;
+
+// substract 
+var priceDifference = onionPrice - eggPrice;
+
+// multiple 
+var eggPriceMultiplication = eggPrice * eggQuantity;
+
+// dividing
+var orangeQuantity = moneyAvailable / orangePrice;
+
+console.log(onionPrice);
+console.log(eggPrice);
+console.log(onionPrice + eggPrice);
+console.log(totalPrice);
+console.log(priceDifference)
+console.log(eggPriceMultiplication)
+console.log(orangeQuantity)
