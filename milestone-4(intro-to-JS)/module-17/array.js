9ea728c25e1bb48994f2057c667f95087f009f82
Monirul Fahad <immonirulfahad@gmail.com>
2021-08-01 11:08:00 +0600

    array

---
+++ milestone-4(intro-to-JS)/module-17/array.js
@@ -0,0 +1,11 @@
+var friendsAge = [19, 13, 21];
+var muriMakhFee = [23, 150, 17, 53];
+
+var nayikas = ['mahi', 'opu', 'Shabnoor', 'Kopila'];
+
+var vowels =[ 'a', 'e', 'i', 'o', 'u']
+
+// console.log(vowels.length)
+
+var vowelsCount = vowels.length
+console.log(vowelsCount)
