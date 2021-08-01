d9b0c03da5c136ca29b2ac373ce4e81a7586d926
Monirul Fahad <immonirulfahad@gmail.com>
2021-08-01 12:39:01 +0600

    index

---
+++ milestone-4(intro-to-JS)/module-17/array-index.js
@@ -0,0 +1,14 @@
+var bookpage = ['hablu', 'bolod', 'choddogosti', 'battery', 'peracitamol']
+
+
+
+var batteryIndex = bookpage.indexOf('battery');
+console.log(batteryIndex)
+
+console.log(bookpage[4])
+
+var numbers = [34, 45, 13, 67, 97]
+console.log(numbers)
+numbers[3] = 40;
+numbers[1] = 7;
+console.log(numbers)
