bb8d84295352da7755b3889de600b03eed5ae74d
Monirul Fahad <immonirulfahad@gmail.com>
2021-08-01 16:51:59 +0600

    if-else

---
+++ milestone-4(intro-to-JS)/module-17/conditional.js
@@ -0,0 +1,22 @@
+var isFoodReady = true;
+
+if (isFoodReady == true) {
+    console.log('Ammu.. I will eat now.')
+}
+
+var iphonePrice = 70000;
+var myBudget = 170000;
+
+if (iphonePrice < myBudget) {
+    console.log('Iphone kinbo')
+}
+
+// if else 
+
+var chickerPrice = 180;
+var myMoney = 80;
+if (chickerPrice < myMoney) {
+    console.log('yes!! murgir ran khamu.')
+} else {
+    console.log('Smash Potato with lentil soup')
+}
