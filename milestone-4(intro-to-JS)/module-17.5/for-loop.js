51419bbbbc801f51a377d02b4ce36103d99097b0
Monirul Fahad <immonirulfahad@gmail.com>
2021-08-02 17:46:23 +0600

    loop

---
+++ milestone-4(intro-to-JS)/module-17.5/for-loop.js
@@ -0,0 +1,37 @@
+// for(var roast = 0; roast < 7; roast++){
+//     console.log('roast den, please...')
+//     console.log(roast)
+// }
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+
+// ood number 
+// for (var i = 1; i < 30; i+=2) {
+// console.log(i)
+// }
+
+// even number 
+// for (var i = 0; i <= 30; i+=2){
+//     console.log(i)
+// }
+
+// var number = [45, 76, 56, 19, 98, 70, 450]
+// for (var i = 0; i < 7; i++) {
+//     var element = number[i]
+//     console.log(element)
+// }
+
+var items = ['car', 'watch', 'book', 'note', 'coca cola', 'mouse', 'bottle', 'water' ]
+
+for (var i = 0; i < items.length; i++){
+    var element = items[i];
+    console.log(element)
+}
+
+
+// var num1 = '99.5';
+// var num2 = 15;
+
+// console.log(num1 + num2)
---
+++ milestone-4(intro-to-JS)/module-17.5/while-loop.js
@@ -0,0 +1,26 @@
+// var roast = 0;
+// while (roast < 7) {
+//     console.log(roast)
+//     console.log('Roast den, Please!!');
+//     roast++
+// }
+
+// var number = 1;
+//  while(number <= 10) {
+//      console.log(number);
+//      number++
+//  }
+
+// odd Number 
+// var number = 1;
+//  while(number <= 20) {
+//      console.log(number);
+//      number = number + 2
+//  }
+// even number 
+
+var number = 0;
+ while(number <= 20) {
+     console.log(number);
+     number = number + 2
+ }
