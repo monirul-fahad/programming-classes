e6859b62bda35d9616986da3e648f8e84817d5ea
Monirul Fahad <immonirulfahad@gmail.com>
2021-08-03 18:34:41 +0600

    switch

---
+++ milestone-4(intro-to-JS)/module-18(function)/switch.js
@@ -0,0 +1,24 @@
+var color = 'yellow';
+
+switch(color){
+    case 'blue':
+    console.log( 'color is blue');
+    break;
+    case 'red':
+    console.log( 'color is red');
+    break;
+    case 'white':
+    console.log( 'color is white');
+    break;
+    case 'green':
+    console.log( 'color is green');
+    break;
+    case 'yellow':
+    console.log( 'color is yellow');
+    break;
+    case 'black':
+    console.log( 'color is black');
+    break;
+    default:
+        console.log('color is megenta')
+}
