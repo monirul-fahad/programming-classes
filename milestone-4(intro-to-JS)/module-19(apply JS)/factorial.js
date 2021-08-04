ce7e1843b47a8f6cf28384c9e0fc7dcec2a70b3e
Monirul Fahad <immonirulfahad@gmail.com>
2021-08-04 16:49:22 +0600

    factorial

---
+++ milestone-4(intro-to-JS)/module-19(apply JS)/factorial.js
@@ -0,0 +1,22 @@
+// factorial in math 
+/*
+3! = 3 X 2 X 1
+4! = 4 X 3 X 2 X 1
+6! = 6 X 5 X 4 X 3 X 2 X 1
+7! = 7 X 6 X 5 X 4 X 3 X 2 X 
+*/
+var factorial = 1;
+for (var i = 1; i <= 7; i++){
+   
+    factorial = factorial * i;
+   
+}
+console.log('factorial of 7 = ', factorial)
+
+
+var factorial2 = 1 ;
+for (var i = 1; i <= 5; i++ ) {
+ factorial2 = factorial2 * i;
+}
+console.log('factorial of 7 = ',factorial2)
+
