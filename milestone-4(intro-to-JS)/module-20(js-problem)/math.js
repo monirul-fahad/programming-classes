c93c325dcefb493eadae34d54890d9ef32095964
Monirul Fahad <immonirulfahad@gmail.com>
2021-08-06 15:22:40 +0600

    module-20

---
+++ milestone-4(intro-to-JS)/module-20(js-problem)/math.js
@@ -0,0 +1,33 @@
+// Math.abs 
+// var myNumber = -5;
+// var output = Math.abs(myNumber);
+// console.log(output)
+
+
+// Math.ceil 
+// const myNumber = 34.00544;
+// const output = Math.ceil(myNumber);
+// console.log(output);
+
+// Math.floor 
+// const myNumber = 34.00544;
+// const output = Math.floor(myNumber);
+// console.log(output);
+
+// Math.round
+// const myNumber = 34.444;
+// const output = Math.round(myNumber);
+// console.log(output);
+
+// Math.random
+// const output = Math.random() * 6;
+// const rounded = Math.round(output)
+// console.log(rounded);
+
+
+// ludu board 
+for (let i = 0; i <=20 ; i++) {
+    const output = Math.random() * 6;
+const rounded = Math.round(output)
+console.log(rounded);
+}
---
+++ milestone-4(intro-to-JS)/module-20(js-problem)/swap.js
@@ -0,0 +1,16 @@
+var first = 5;
+var second = 7;
+// console.log(first, second)
+
+// first approch to swap varible values.
+// var temp = first; 
+// first = second;
+// second = temp;
+// console.log(first, second)
+
+
+// destructuring 
+
+[first, second] = [second, first]
+console.log(first, second)
+
