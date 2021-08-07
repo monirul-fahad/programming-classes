0bf572bda23ab90bf1e875dc29796656306b739d
Monirul Fahad <immonirulfahad@gmail.com>
2021-08-07 18:31:38 +0600

    last push

---
+++ milestone-4(intro-to-JS)/module-21(more-js-problem)/travelling.js
@@ -0,0 +1,10 @@
+function animalCount(miles) {
+    if (miles ) {
+        const count = miles * 10;
+        return count;
+    } else if (miles <= 20) {
+        const first10  = 10;
+    }
+}
+const animals = animalCount(8);
+console.log(animals)
