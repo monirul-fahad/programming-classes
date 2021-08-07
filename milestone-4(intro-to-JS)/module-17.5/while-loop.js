58ed3f59397373dcfa30c46905971275edaab13b
Monirul Fahad <immonirulfahad@gmail.com>
2021-08-07 22:40:08 +0600

    last push

--- milestone-4(intro-to-JS)/module-17.5/while-loop.js
+++ milestone-4(intro-to-JS)/module-17.5/while-loop.js
@@ -19,8 +19,15 @@
 //  }
 // even number 
 
-var number = 0;
- while(number <= 20) {
-     console.log(number);
-     number = number + 2
+// var number = 0;
+//  while(number <= 20) {
+//      console.log(number);
+//      number = number + 2
+//  }
+
+ let sum = 0;
+ for (let i = 0; i <=3; i++){
+     sum = sum + i;
  }
+
+ console.log(sum);
