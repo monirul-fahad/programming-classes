b7fdd2be0eaafa325dd7eecbc7e595545b6dd680
Monirul Fahad <immonirulfahad@gmail.com>
2021-08-19 14:18:25 +0600

    push

---
+++ milestone-5(JS-relation-html)/module-28(string-array)/case-convert.js
@@ -0,0 +1,10 @@
+const anthem = 'amar shonar bangla ami tomay valobashi';
+const search = 'valo';
+
+
+const userInput = 'blackpinK';
+const savedUserName = 'blackPink';
+if (userInput.toLowerCase() == savedUserName.toLowerCase()) {
+    console.log('user exxits')
+}
+
