32b5913c18a0bcfb3f8463802988513e7da6bbfb
Monirul Fahad <immonirulfahad@gmail.com>
2021-08-23 16:46:46 +0600

    push

---
+++ milestone-6(ES6-API)/module-30(ES6-Intro)/spread.js
@@ -0,0 +1,18 @@
+const numbers = [23, 56, 78, 12, 98]
+// console.log(numbers);
+// console.log(...numbers);
+
+//simple max number find. 
+const max = Math.max(23, 99, 65);
+console.log(max)
+
+//max number from array. //spread operator
+const maxInArray = Math.max(...numbers);
+console.log(maxInArray)
+
+//spread operator
+//adding number array in another array. 
+const numbers2 = [...numbers]
+// numbers.push(55)
+console.log(numbers2);
+
