47a53feff895348dc4ce44465fa19d64c4795ba0
Monirul Fahad <immonirulfahad@gmail.com>
2021-08-24 10:16:58 +0600

    yes

---
+++ milestone-6(ES6-API)/module-31(more-ES6)/1-destructuring.js
@@ -0,0 +1,13 @@
+const fish = {
+    name: 'king Hilsha',
+    id: 58,
+    price: 9000,
+    phone: '01754353464',
+    address: 'chadpur',
+    waight: 4
+};
+
+const phone = fish.phone;
+console.log(phone);
+
+const {phone, price, waight, id, name} = fish;
