6c3b68432208354e1ae326bdc816011e916202f6
Monirul Fahad <immonirulfahad@gmail.com>
2021-08-26 18:24:20 +0600

    todays last

---
+++ milestone-6(ES6-API)/module-32(API-intro)/practice/comment.js
@@ -0,0 +1,20 @@
+function loadComment(){
+    fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(res => res.json())
+    .then(data => comments(data))
+};
+loadComment()
+
+function comments(comments){
+    const commentContainer = document.getElementById('comments');
+    for (const comment of comments) {
+        const div = document.createElement('div');
+        div.classList.add('comment')
+        div.innerHTML = `
+        <h3> Name: ${comment.name}</h3>
+        <h4> email: ${comment.email}</h4>
+        <p>${comment.body}</p>
+        `;
+        commentContainer.appendChild(div)
+    }
+}
