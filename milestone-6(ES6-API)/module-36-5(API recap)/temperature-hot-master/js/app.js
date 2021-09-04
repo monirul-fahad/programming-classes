c796e4475e0c4fea88d8d3fbaa394fb7cd69e30e
Monirul Fahad <immonirulfahad@gmail.com>
2021-09-04 15:17:39 +0600

    weather

---
+++ milestone-6(ES6-API)/module-36-5(API recap)/temperature-hot-master/js/app.js
@@ -0,0 +1,23 @@
+const API_KEY = `b456d7e405795f9e5fab94eee95c707b`;
+
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayTemperature(data))
+};
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text; 
+}
+
+const displayTemperature = (temperature) => {
+    setInnerText('city', temperature.name)
+    setInnerText('temperature', temperature.main.temp)
+    setInnerText('condition', temperature.weather[0].main)
+    // set weather icon
+    const url = `https://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('w-icon');
+    imgIcon.setAttribute('src', url)
+}
